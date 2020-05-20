7c660ab25bf1bdebfe73a79497ebcd57abb426a4
jsong073 <jaehasong073@gmail.com>
2020-05-20 16:37:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Exercise Using Conditional Statements 
---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private String genre;
+    private boolean inStock;
+    private int rating;
+    private String year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+}
